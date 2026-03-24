624cef75f7b7f6c2f4ed728746bee540740ffc6f
francis2016e <emechidiebere1998@gmail.com>
2026-03-24 03:08:14 +0000

    Fix: Routing and base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import { createBrowserRouter,createRoutesFromElements,RouterProvider,Route } from 'react-router-dom'
+import { createHashRouter,createRoutesFromElements,RouterProvider,Route } from 'react-router-dom'
 import MainLayout from './layout/MainLayout'
 import HomePage from './pages/HomePage'
-import Terminals from './component/Terminals'
+
 import TerminalsLocations from './component/TerminalsLocation'
 import WarningPage from './pages/WarningPage'
 import ServicePage from './pages/ServicePage'
 import AboutPage from './pages/AboutPage'
 
 
+
 function App() {
-  const router = createBrowserRouter(
+  const router = createHashRouter(
     createRoutesFromElements(
+      
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/terminals" element={<TerminalsLocations />} />
@@ -21,6 +23,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
 
       </Route>
+      
       
     )
   )
