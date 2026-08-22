bb5bb22dca2414815571c0658c29323ab2d9b3c4
francis2016e <emechidiebere1998@gmail.com>
2026-08-22 13:51:03 +0100

    Fix terminal image paths

--- src/assets/terminal.js
+++ src/assets/terminal.js
@@ -1,5 +1,5 @@
-import terminal1 from './terminal 1.webp';
-import terminal2 from './terminal 2 ticketing.jfif';
+import terminal1 from './terminal 1.jfif';
+import terminal2 from './Terminal 2 ticketing.jfif';
 import terminal1B from './terminal 1.jfif';
 import holyGhost from './Holy-Ghost-terminal T2.webp';
 import terminalHall from './enugu bus terminal hall.webp';
